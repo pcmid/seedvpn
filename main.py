7851777f02653ef9b07d26d55afd52e97394172c
sweet-st <foreverofsweet@gmail.com>
2017-08-24 13:31:34 +0800

    修复一个错误

--- main.py
+++ main.py
@@ -206,7 +206,27 @@ class DHCP():
         
         
 def usage(status = ARGS_ERROR):
-    print("Usage: %s [-s port|-c serverip] [-h] [-l localip]" % (sys.argv[0]))
+    print("Usage: %s [-s port|-c serverip] [-h] [-l localip]\n\n" % (sys.argv[0]))
+    print("""           seedvpn  Copyright (C) 2017  sweet-st    
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+
+The hypothetical commands `show w' and `show c' should show the appropriate
+parts of the General Public License.  Of course, your program's commands
+might be different; for a GUI interface, you would use an "about box".
+
+  You should also get your employer (if you work as a programmer) or school,
+if any, to sign a "copyright disclaimer" for the program, if necessary.
+For more information on this, and how to apply and follow the GNU GPL, see
+<http://www.gnu.org/licenses/>.
+
+  The GNU General Public License does not permit incorporating your program
+into proprietary programs.  If your program is a subroutine library, you
+may consider it more useful to permit linking proprietary applications with
+the library.  If this is what you want to do, use the GNU Lesser General
+Public License instead of this License.  But first, please read
+<http://www.gnu.org/philosophy/why-not-lgpl.html>.""")
     sys.exit(status)
 
 
@@ -231,7 +251,8 @@ if __name__=="__main__":
 
     tun = Tunnel()
     tun.create()
-    ifConfig = Ifconfig(tun.tname)
+    if MODE == 1:
+        tun.config(IFACE_IP)
     try:
         tun.run()
     except KeyboardInterrupt:
